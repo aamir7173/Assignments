e9f469a098fe3364202b293606b522d029c86d85
Aamir shohail <90526868+aamir7173@users.noreply.github.com>
2022-06-03 16:56:03 +0530

    Add files via upload

---
+++ Python Basic, Conditional Loops.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[47]:
+
+
+print([x for x in range(2000,3200) if (x%7==0 and x%5!=0)])
+
+
+# In[76]:
+
+
+firstname = input("Input your First Name :")
+lastname = input("Input your Last Name :")
+print (lastname," ",firstname)
+
+
+# In[69]:
+
+
+r= 6.0
+pi = 3.1415
+V= 4.0/3.0*pi* r**3
+print('The volume of the sphere is: ',V)
+
